fb709441336a51617f1b4e84d53bb2ebf39982d2
lateSWE <35782260+lateSWE@users.noreply.github.com>
2019-04-29 16:10:28 +0200

    dunno

--- src/main.java
+++ src/main.java
@@ -8,48 +8,67 @@ public class main {
     public static void main(String [] args){
         String filename = new String();
         Scanner input = new Scanner(System.in);
+        Boolean run = true;
 
+        while (true) {
+            System.out.println(
+                    "[1] - Most common words (Slow loading time)\n" +
+                            "[2] - Only nouns\n" +
+                            "[3] - Own text file(must end with \".txt\"!");
+            int ChoiceOne = input.nextInt();
+            switch (ChoiceOne) {
+                case (1):
+                    filename = "wiki-100k.txt";
+                    System.out.println("Loading words and choosing one...");
+                    break;
 
-        System.out.println(
-                "[1] - Most common words (Slow loading time)\n" +
-                "[2] - Only nouns\n" +
-                "[3] - Own text file(must end with \".txt\"!");
-        int ChoiceOne = input.nextInt();
-        switch (ChoiceOne){
-            case(1):
-                filename = "wiki-100k.txt";
-                System.out.println("Loading words and choosing one...");
-                break;
+                case (2):
+                    filename = "nouns.txt";
+                    System.out.println("Loading words and choosing one...");
+                    break;
 
-            case(2):
-                filename = "nouns.txt";
-                System.out.println("Loading words and choosing one...");
-                break;
+                case (3):
+                    filename = input.nextLine();
+                    System.out.println("Loading words and choosing one");
+                    break;
+                default:
+                    System.out.println("Error!!!! try again\n");
+                    break;
+            }
+            while (run==true) {
+                //Randomizing the word  |
+                //                      V
+                System.out.println("\n\n");
+                int ChoisePC = (int) RandomWord(ReadFile(filename).size());
+                for (int i = 0; i < ReadFile(filename).get(ChoisePC).length(); i++) {
+                    System.out.print(" _");
+                }
+                System.out.println();
+                System.out.println(ReadFile(filename).get(ChoisePC));
 
-            case (3):
-                filename = input.nextLine();
-                System.out.println("Loading words and choosing one");
-                break;
-            default:
-                System.out.println("Error!!!! try again\n");
-                break;
-        }
+                //Putting chosen word into a ArrayList  |
+                //                                      V
 
-        //Randomizing the word  |
-        //                      V
-        System.out.println("\n\n");
-        int ChoisePC = (int) RandomWord(ReadFile(filename).size());
-        for (int i = 0; i < ReadFile(filename).get(ChoisePC).length(); i++) {
-            System.out.print(" _");
-        }
-        System.out.println();
-        System.out.println(ReadFile(filename).get(ChoisePC));
+                ArrayList<String> RandomizedWord = new ArrayList<>();
+                for (int i = 0; i < ReadFile(filename).get(ChoisePC).length(); i++) {
+                    RandomizedWord.add(Character.toString(ReadFile(filename).get(ChoisePC).charAt(i)));
+                }
+                //Guessing part  |
+                //               V
+                char GuessUserChar = 'a';
+                while (true) {
+                    String GuessUser = input.nextLine();
+                    if (GuessUser.length() == 1) {
+                        GuessUserChar = GuessUser.charAt(1);
+                        break;
+                    }
+                }
+                for (int i = 0; i < ReadFile(filename).get(ChoisePC).length(); i++) {
+                    if (GuessUserChar == ReadFile(filename).get(ChoisePC).charAt(i)) {
 
-        //Guessing part  |
-        //               V
-        String GuessUser = input.nextLine();
-        for (int i = 0; i < ReadFile(filename).get(ChoisePC).length(); i++) {
-            if ()
+                    }
+                }
+            }
         }
     }
     private  static double RandomWord(int i){
